985d43b2ff14d48c4b535383b4c128218a596426
@liusy <1406618174@qq.com>
2023-08-25 19:48:39 +0800

    生成后

---
+++ service/service_edu/src/main/java/com/lsy/edu/mapper/TeacherMapper.java
@@ -0,0 +1,16 @@
+package com.lsy.edu.mapper;
+
+import com.lsy.edu.entity.Teacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author lsy
+ * @since 2023-08-25
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
---
+++ service/service_edu/src/main/java/com/lsy/edu/service/TeacherService.java
@@ -0,0 +1,16 @@
+package com.lsy.edu.service;
+
+import com.lsy.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author lsy
+ * @since 2023-08-25
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
